757824a17cdfdf5d63b555607927401ac8bd6049
gabricp <gabriel22t@hotmail.com>
2024-12-09 10:50:18 +0100

    Reorganización en un notebook

--- models/u_net.py
+++ models/u_net.py
@@ -3,6 +3,64 @@ import numpy as np
 import torch
 
 
+class Conv_3_k(nn.Module):
+    def __init__(self, channels_in, channels_out):
+        super().__init__()
+        self.conv1 = nn.Conv2d(channels_in, channels_out, kernel_size=3, stride=1, padding=1)
+    def forward(self, x):
+        return self.conv1(x)
+    
+class Double_Conv(nn.Module):
+    '''
+    Double convolution block for U-Net
+    '''
+    def __init__(self, channels_in, channels_out):
+        super().__init__()
+        self.double_conv = nn.Sequential(
+                           Conv_3_k(channels_in, channels_out),
+                           nn.BatchNorm2d(channels_out),
+                           nn.ReLU(),
+            
+                           Conv_3_k(channels_out, channels_out),
+                           nn.BatchNorm2d(channels_out),
+                           nn.ReLU(),
+                            )
+    def forward(self, x):
+        return self.double_conv(x)
+    
+class Down_Conv(nn.Module):
+    '''
+    Down convolution part
+    '''
+    def __init__(self, channels_in, channels_out):
+        super().__init__()
+        self.encoder = nn.Sequential(
+                        nn.MaxPool2d(2,2),
+                        Double_Conv(channels_in, channels_out)
+                        )
+    def forward(self, x):
+        return self.encoder(x)
+    
+class Up_Conv(nn.Module):
+    '''
+    Up convolution part
+    '''
+    def __init__(self,channels_in, channels_out):
+        super().__init__()
+        self.upsample_layer = nn.Sequential(
+                        nn.Upsample(scale_factor=2, mode='bicubic'),
+                        nn.Conv2d(channels_in, channels_in//2, kernel_size=1, stride=1)
+                        )
+        self.decoder = Double_Conv(channels_in, channels_out)
+    
+    def forward(self, x1, x2):
+        '''
+        x1 - upsampled volume
+        x2 - volume from down sample to concatenate
+        '''
+        x1 = self.upsample_layer(x1)
+        x = torch.cat([x2, x1],dim=1)
+        return self.decoder(x)
 class UNET(nn.Module):
     def __init__(self, input_shape):
         super(UNET,self).__init__()
